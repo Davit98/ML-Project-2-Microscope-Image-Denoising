228dc24a1cf526ec2667667be3fb36596e8a7be4
Oganes Manasian <oganesman8@mail.ru>
2019-12-16 00:39:52 +0200

    Revert "Refactored code"
    This reverts commit 8081c3f874be5b97b67de9a8c1da4a97866e1b4f.

--- train.py
+++ train.py
@@ -28,12 +28,16 @@ def get_args():
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--source_image_dir", type=str, required=True,
                         help="source image dir")
+
     parser.add_argument("--target_image_dir", type=str, required=True,
                         help="target image dir")
+
     parser.add_argument("--source_val_dir", type=str, required=True,
                         help="validation set source image dir")
+
     parser.add_argument("--target_val_dir", type=str, required=True,
                         help="validation set target image dir")
+
     parser.add_argument("--image_size", type=int, default=512,
                         help="training patch size")
     parser.add_argument("--batch_size", type=int, default=16,
@@ -48,10 +52,10 @@ def get_args():
                         help="loss; mse', 'mae', or 'l0' is expected")
     parser.add_argument("--weight", type=str, default=None,
                         help="weight file for restart")
-    parser.add_argument("--output_path", type=str, default="weights",
-                        help="weight dir")
+    parser.add_argument("--output_path", type=str, default="checkpoints",
+                        help="checkpoint dir")
     parser.add_argument("--model", type=str, default="n2n_unet",
-                        help="model architecture, possible options 'n2n_unet'")
+                        help="model architecture ('n2n_unte' or 'srresnet' or 'unet')")
     args = parser.parse_args()
 
     return args
@@ -59,58 +63,54 @@ def get_args():
 
 def main():
     args = get_args()
-
-    # Files and paths
     source_image_dir = args.source_image_dir
     target_image_dir = args.target_image_dir
+
     source_val_dir = args.source_val_dir
     target_val_dir = args.target_val_dir
-    output_path = Path(__file__).resolve().parent.joinpath(args.output_path)
-    output_path.mkdir(parents=True, exist_ok=True)
 
-    # Model parameters
     image_size = args.image_size
     batch_size = args.batch_size
     nb_epochs = args.nb_epochs
     lr = args.lr
+    steps = args.steps
     loss_type = args.loss
-
-    # Build model
+    output_path = Path(__file__).resolve().parent.joinpath(args.output_path)
     model = get_model(args.model)
+
     if args.weight is not None:
         model.load_weights(args.weight)
+
     opt = Adam(lr=lr)
+    callbacks = []
 
-    callbacks = []  # TODO move on some lines below
-
-    # if loss_type == "l0": # TODO delete
-    #     l0 = L0Loss()
-    #     callbacks.append(UpdateAnnealingParameter(l0.gamma, nb_epochs, verbose=1))
-    #     loss_type = l0()
+    if loss_type == "l0":
+        l0 = L0Loss()
+        callbacks.append(UpdateAnnealingParameter(l0.gamma, nb_epochs, verbose=1))
+        loss_type = l0()
 
     model.compile(optimizer=opt, loss=loss_type, metrics=[PSNR])
+    generator = TrainImageGenerator(source_image_dir, target_image_dir, batch_size=batch_size, image_size=image_size)
+    
+    val_generator = ValGenerator(source_val_dir,target_val_dir)
 
+    output_path.mkdir(parents=True, exist_ok=True)
     callbacks.append(LearningRateScheduler(schedule=Schedule(nb_epochs, lr)))
-    callbacks.append(ModelCheckpoint(str(output_path) + "/weights.{epoch:03d}-{val_loss:.3f}.hdf5",
+    callbacks.append(ModelCheckpoint(str(output_path) + "/weights.{epoch:03d}-{val_loss:.3f}-{val_PSNR:.5f}.hdf5",
                                      monitor="val_loss",
                                      verbose=1,
-                                     period=10)
-                     )
+                                     period = 10)
+                                     # mode="min",
+                                     # save_best_only=True)
+                    )
 
-    # Add tensorboard logs
+    # tensorboard --logdir='logs/'
     NAME = "Noise2Noise-Biology-{}".format(int(time.time()))
-    tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+    tensorboard = TensorBoard(log_dir = 'logs/{}'.format(NAME))
     callbacks.append(tensorboard)
 
-    # Form train and validation sets
-    train_generator = TrainImageGenerator(source_image_dir,
-                                          target_image_dir,
-                                          batch_size=batch_size, image_size=image_size)
-    val_generator = ValGenerator(source_val_dir,
-                                 target_val_dir)
-
-    # Train
-    hist = model.fit_generator(generator=train_generator,
+    hist = model.fit_generator(generator=generator,
+                               # steps_per_epoch=steps,
                                epochs=nb_epochs,
                                validation_data=val_generator,
                                verbose=1,
