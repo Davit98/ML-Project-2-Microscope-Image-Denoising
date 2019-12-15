8081c3f874be5b97b67de9a8c1da4a97866e1b4f
Oganes Manasian <oganesman8@mail.ru>
2019-12-15 21:51:55 +0200

    Refactored code
    Added comments
    Moved preprocessing to .py format

--- generator.py
+++ generator.py
@@ -1,17 +1,19 @@
 from pathlib import Path
-import random
 import numpy as np
 import cv2
 from keras.utils import Sequence
 
+IMAGE_FORMATS = (".jpeg", ".jpg", ".png", ".bmp")
+
 
 class TrainImageGenerator(Sequence):
     def __init__(self, source_image_dir, target_image_dir, batch_size=32, image_size=512):
-        image_suffixes = (".jpeg", ".jpg", ".png", ".bmp")
-        self.source_image_paths = [p for p in sorted(Path(source_image_dir).glob("**/*")) if p.suffix.lower() in image_suffixes]
-        self.target_image_paths = [p for p in sorted(Path(target_image_dir).glob("**/*")) if p.suffix.lower() in image_suffixes]
+        self.source_image_paths = [p for p in sorted(Path(source_image_dir).glob("**/*")) if
+                                   p.suffix.lower() in IMAGE_FORMATS]
+        self.target_image_paths = [p for p in sorted(Path(target_image_dir).glob("**/*")) if
+                                   p.suffix.lower() in IMAGE_FORMATS]
 
-        if len(self.source_image_paths)!=len(self.target_image_paths): 
+        if len(self.source_image_paths) != len(self.target_image_paths):
             raise ValueError("The number of source images is not equal to that of target images")
 
         self.image_num = len(self.source_image_paths)
@@ -23,43 +25,39 @@ class TrainImageGenerator(Sequence):
         return self.image_num // self.batch_size
 
     def __getitem__(self, idx):
-
         batch_size = self.batch_size
         image_size = self.image_size
         x = np.zeros((batch_size, image_size, image_size, 1), dtype=np.uint8)
         y = np.zeros((batch_size, image_size, image_size, 1), dtype=np.uint8)
 
-        for k in range(idx*batch_size,(idx+1)*batch_size):
-
+        for k in range(idx * batch_size, (idx + 1) * batch_size):
             source_image_path = self.source_image_paths[k]
-            source_image = cv2.imread(str(source_image_path),cv2.IMREAD_GRAYSCALE).reshape(image_size,image_size,1)
+            source_image = cv2.imread(str(source_image_path), cv2.IMREAD_GRAYSCALE).reshape(image_size, image_size, 1)
 
             target_image_path = self.target_image_paths[k]
-            target_image = cv2.imread(str(target_image_path),cv2.IMREAD_GRAYSCALE).reshape(image_size,image_size,1)
+            target_image = cv2.imread(str(target_image_path), cv2.IMREAD_GRAYSCALE).reshape(image_size, image_size, 1)
 
-            x[k-idx*batch_size] = source_image
-            y[k-idx*batch_size] = target_image
-                
+            x[k - idx * batch_size] = source_image
+            y[k - idx * batch_size] = target_image
+
         return x, y
 
 
 class ValGenerator(Sequence):
     def __init__(self, val_source_dir, val_target_dir):
-        image_suffixes = (".jpeg", ".jpg", ".png", ".bmp")
-
-        image_path_source = [p for p in sorted(Path(val_source_dir).glob("**/*")) if p.suffix.lower() in image_suffixes]
-        image_path_target = [p for p in sorted(Path(val_target_dir).glob("**/*")) if p.suffix.lower() in image_suffixes]
+        image_path_source = [p for p in sorted(Path(val_source_dir).glob("**/*")) if p.suffix.lower() in IMAGE_FORMATS]
+        image_path_target = [p for p in sorted(Path(val_target_dir).glob("**/*")) if p.suffix.lower() in IMAGE_FORMATS]
 
         self.image_num = len(image_path_source)
         self.data = []
 
         for i in range(self.image_num):
-            x = cv2.imread(str(image_path_source[i]),cv2.IMREAD_GRAYSCALE)
-            y = cv2.imread(str(image_path_target[i]),cv2.IMREAD_GRAYSCALE)
+            x = cv2.imread(str(image_path_source[i]), cv2.IMREAD_GRAYSCALE)
+            y = cv2.imread(str(image_path_target[i]), cv2.IMREAD_GRAYSCALE)
 
-            x = x.reshape(1,x.shape[0],x.shape[1],1)
-            y = y.reshape(1,y.shape[0],y.shape[1],1)
-            self.data.append([x,y])
+            x = x.reshape(1, x.shape[0], x.shape[1], 1)
+            y = y.reshape(1, y.shape[0], y.shape[1], 1)
+            self.data.append([x, y])
 
     def __len__(self):
         return self.image_num
--- test.py
+++ test.py
@@ -11,11 +11,11 @@ def get_args():
     parser.add_argument("--image_dir", type=str, required=True,
                         help="test image dir")
     parser.add_argument("--model", type=str, default="n2n_unet",
-                        help="model architecture ('n2n_unet' or'srresnet' or 'unet')")
+                        help="model architecture, possible options 'n2n_unet'")
     parser.add_argument("--weight_file", type=str, required=True,
                         help="trained weight file")
-    parser.add_argument("--output_dir", type=str, default=None,
-                        help="if set, save resulting images otherwise show result using imshow")
+    parser.add_argument("--output_dir", type=str, required=True,
+                        help="folder to save the resulting omages")
     args = parser.parse_args()
     return args
 
@@ -32,38 +32,27 @@ def main():
     model = get_model(args.model)
     model.load_weights(weight_file)
 
-    if args.output_dir:
-        output_dir = Path(args.output_dir)
-        output_dir.mkdir(parents=True, exist_ok=True)
+    output_dir = Path(args.output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
 
     image_paths = list(Path(image_dir).glob("*.*"))
 
     for image_path in image_paths:
-        image = cv2.imread(str(image_path),cv2.IMREAD_GRAYSCALE)
+        image = cv2.imread(str(image_path), cv2.IMREAD_GRAYSCALE)
         h, w = image.shape
-        image = image.reshape(h,w,1)
+        image = image.reshape(h, w, 1)
 
         out_image = np.zeros((h, w * 2, 1), dtype=np.uint8)
 
         pred = model.predict(np.expand_dims(image, 0))
         denoised_image = get_image(pred[0])
 
+        # Place denoised image next to original one
         out_image[:, :w] = image
         out_image[:, w:w * 2] = denoised_image
 
-        if args.output_dir:
-            cv2.imwrite(str(output_dir.joinpath(image_path.name))[:-4] + ".png", out_image)
-        else:
-            cv2.imshow("result", out_image)
-            key = cv2.waitKey(-1)
-            # "q": quit
-            if key == 113:
-                return 0
+        cv2.imwrite(str(output_dir.joinpath(image_path.name))[:-4] + ".png", out_image)
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
